e0181197d85fdd94fec2e0ed699cee293f6a495b
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-03-23 22:17:23 +0300

    Commit

---
+++ jq002/lesson011.js
@@ -0,0 +1,10 @@
+$(function(){
+    $("button").click(function(){
+        $("div").animate({
+            left: '250px',
+            opacity: '0.5',
+            height: '150px',
+            width: '150px'
+        })
+    })
+})
